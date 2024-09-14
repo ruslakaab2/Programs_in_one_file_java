169ab136f79ca2349fdf0d910193b3d884235dc2
ruslakaab2 <149611623+ruslakaab2@users.noreply.github.com>
2024-09-14 21:05:07 +0500

    Update Main.java

--- programm_java/shop_sistem/src/Main.java
+++ programm_java/shop_sistem/src/Main.java
@@ -3,16 +3,12 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-        Person hh= new Person("тимур",15,"71998234345",50,1500);
-        Item jj=new Item("чипсы",1500,5);
-        hh.buy_item(jj);
-        hh.zaplata();
-        hh.zaplata();
-        hh.zaplata();
-        hh.buy_item(jj);
-        hh.buy_item(jj);
-        hh.buy_item(jj);
-
+        Person hh= new Person("noname",15,"71998234345",50,1500);
+        Item jj=new Item("Cola",50,5);
+        hh.buy_item(jj);//покупка предмета 
+        hh.zaplata();//заплата
+        hh.buy_item(jj);//покупка предмета
+        
     }
 
 }
